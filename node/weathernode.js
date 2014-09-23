f49bbad8f19fcaaef6a64b9f864f9df639500cc2
Christian Kuster <christian.kuster@kaba.com>
2014-09-23 15:25:02 +0200

    Runs now very basic on the BeagleBone :-)

--- node/weathernode.js
+++ node/weathernode.js
@@ -1,19 +1,27 @@
+/***********************************************************************************/
 /**
  * Created by Christian on 22.09.2014.
  */
+/***********************************************************************************/
 
 var weather = require("./weatherrecord.js");
 
-var serialComPort = "COM4";
-var serialComPort = "/dev/ttyUSB0"
+// Configure the comport as required by your system
+var serialComPort = "COM4";         // Windows
+var serialComPort = "/dev/ttyUSB0"  // Linux
 
+/***********************************************************************************/
+// Webserver part
 var http = require("http");
 var url = require("url");
 var path = require("path");
 var fs = require("fs");
 var port = process.argv[2] || 8888;
 
-http.createServer(function(request, response) {
+/**
+ * Webserver instance
+ */
+http.createServer(function (request, response) {
 
   var uri = url.parse(request.url).pathname;
   var filename = path.join(process.cwd() + '/html', uri);
@@ -21,10 +29,8 @@ http.createServer(function(request, response) {
   var getCurrentRecordPattern = /ajax\/current.html/;
 
 
-
-
-  fs.exists(filename, function(exists) {
-    if(!exists) {
+  fs.exists(filename, function (exists) {
+    if (!exists) {
       if (getCurrentRecordPattern.test(uri)) {
         response.writeHead(200);
         response.write(weather.getCurrentRecord());
@@ -42,8 +48,8 @@ http.createServer(function(request, response) {
       filename += '/index.html';
     }
 
-    fs.readFile(filename, "binary", function(err, file) {
-      if(err) {
+    fs.readFile(filename, "binary", function (err, file) {
+      if (err) {
         response.writeHead(500, {"Content-Type": "text/plain"});
         response.write(err + "\n");
         response.end();
@@ -59,43 +65,39 @@ http.createServer(function(request, response) {
       }
 
 
-        response.write(file, "binary");
+      response.write(file, "binary");
 
       response.end();
     });
   });
 }).listen(parseInt(port, 10));
 
-console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
-
-var server = http.createServer(function(req, res) {
-  console.log(req);
-  console.log(req);
-  res.writeHead(200);
-  res.write('<!DOCTYPE html><html><head>');
-  res.write('<meta charset="UTF-8">');
-  res.write('<meta http-equiv="refresh" content="60" >');
-  res.write('</head><body>');
-  res.write('</body></html>');
-  res.write(weather.getCurrentRecord());
-  res.end();
-});
-server.listen(8081);
+console.log("Weather server is running at http://localhost:" + port + "/\nCTRL + C to shutdown");
 
 
+/***********************************************************************************/
+// Serial port handling
+/***********************************************************************************/
+var serialport = require("./node_modules/serialport/serialport.js");
+var SerialPort = serialport.SerialPort; // localize object constructor
 
-
-var SerialPort = require("./node_modules/serialport/serialport.js").SerialPort
-var serialPort = new SerialPort(serialComPort, {
-  baudrate: 9600
+var sp = new SerialPort(serialComPort, {
+  baudrate: 9600,
+  parser  : serialport.parsers.readline("\n")
 }, false); // this is the openImmediately flag [default is true]
 
-serialPort.open(function () {
-  console.log('open');
+/**
+ * Open the serial port
+ */
+sp.open(function () {
+  console.log('Serialport opened');
 
+  // this is just a test record
   var we3 = weather.newRecord("$1;1;;;;;;;;;;;;;;;;;;21,1;50;0,0;10;0;0");
   console.log("E:" + we3.toString());
-  serialPort.on('data', function(data) {
+
+  // handles the incoming data, creates a new weather record
+  sp.on('data', function (data) {
     var r = weather.newRecord(data);
     if (r != null) {
       console.log('data received: ' + r.toString());
