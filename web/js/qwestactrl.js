e19ab1cceb7c5d22a5f9393b2555119316be63a3
Christian Kuster <christian.kuster@kaba.com>
2014-10-29 21:46:44 +0100

    - First tests with responsive design for main page

---
+++ web/js/qwestactrl.js
@@ -0,0 +1,50 @@
+/***********************************************************************************/
+/*
+ File:    qwestactrl.js
+ Purpose: AngularJs controller for the qwesta weather main page
+ Author:  Christian Kuster, CH-8342 Wernetshausen, www.kusti.ch, 29.10.14
+ Github:  https://github.com/ancasicolica/qwesta
+
+ ----------------------------------------------------------------------------------
+
+ This is free and unencumbered software released into the public domain.
+
+ Anyone is free to copy, modify, publish, use, compile, sell, or
+ distribute this software, either in source code form or as a compiled
+ binary, for any purpose, commercial or non-commercial, and by any
+ means.
+
+ In jurisdictions that recognize copyright laws, the author or authors
+ of this software dedicate any and all copyright interest in the
+ software to the public domain. We make this dedication for the benefit
+ of the public at large and to the detriment of our heirs and
+ successors. We intend this dedication to be an overt act of
+ relinquishment in perpetuity of all present and future rights to this
+ software under copyright law.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ OTHER DEALINGS IN THE SOFTWARE.
+
+ For more information, please refer to <http://unlicense.org/>
+
+ */
+/***********************************************************************************/
+
+
+var qwesta = angular.module('qwestaApp', []);
+
+qwesta.controller('QwestaCtrl', ['$scope', '$http', '$interval', function ($scope, $http, $interval) {
+    $scope.test = "img/webtest/webtest00.jpg";
+
+    $scope.set = function (index) {
+      $scope.test = "img/webtest/webtest" + index + ".jpg";
+    }
+  }
+  ]
+);
+
